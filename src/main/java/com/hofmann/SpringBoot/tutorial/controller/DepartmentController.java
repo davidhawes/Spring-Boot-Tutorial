c59b2b72a00e76b9d210a0b6a7f313fd833857ba
davidhawes <dhofmann@tsn.at>
2021-11-15 00:27:10 +0100

    fetchDepartmentList, fetchDepartmentById sowie deleteDepartmentById Methoden

--- src/main/java/com/hofmann/SpringBoot/tutorial/controller/DepartmentController.java
+++ src/main/java/com/hofmann/SpringBoot/tutorial/controller/DepartmentController.java
@@ -3,9 +3,9 @@ package com.hofmann.SpringBoot.tutorial.controller;
 import com.hofmann.SpringBoot.tutorial.entity.Department;
 import com.hofmann.SpringBoot.tutorial.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -16,5 +16,21 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted Successfully!";
     }
 }
--- src/main/java/com/hofmann/SpringBoot/tutorial/service/DepartmentService.java
+++ src/main/java/com/hofmann/SpringBoot/tutorial/service/DepartmentService.java
@@ -2,7 +2,15 @@ package com.hofmann.SpringBoot.tutorial.service;
 
 import com.hofmann.SpringBoot.tutorial.entity.Department;
 
+import java.util.List;
+
 public interface DepartmentService {
 
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId);
+
+    public void deleteDepartmentById(Long departmentId);
 }
