eb5815fb4dd4fb32bf3b57c2389644c1f66a1795
davidhawes <dhofmann@tsn.at>
2021-11-15 01:18:11 +0100

    Validation

--- src/main/java/com/hofmann/SpringBoot/tutorial/entity/Department.java
+++ src/main/java/com/hofmann/SpringBoot/tutorial/entity/Department.java
@@ -1,10 +1,12 @@
 package com.hofmann.SpringBoot.tutorial.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Entity
 public class Department {
