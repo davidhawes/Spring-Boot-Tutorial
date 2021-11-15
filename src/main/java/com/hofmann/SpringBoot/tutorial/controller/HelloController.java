3f8fd8d5155813a9295fff794822ca21cf0a7bf3
davidhawes <dhofmann@tsn.at>
2021-11-15 22:07:44 +0100

    Spring Boot Profiles

--- src/main/java/com/hofmann/SpringBoot/tutorial/controller/HelloController.java
+++ src/main/java/com/hofmann/SpringBoot/tutorial/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.hofmann.SpringBoot.tutorial.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping(value = "/")
     public String helloWorld(){
-        return "Hallo jungs";
+        return welcomeMessage;
     }
 
 }
